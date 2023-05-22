75ae69a35704ce5eae438c020f97eb4c2a4bbf07
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-05-21 22:44:00 -0500

    ver. 1

--- app.js
+++ app.js
@@ -3,18 +3,38 @@ require("dotenv").config();
 const app = express();
 const port = 3000;
 
+const authBasePath = process.env.PINGONE_AUTH_BASE_PATH;
+const envID = process.env.PINGONE_ENV_ID;
+const clientID = process.env.PINGONE_CLIENT_ID;
+const clientSecret = process.env.PINGONE_CLIENT_SECRET;
+const scope = "openid profile p1:read:user";
+const responseType = "code";
+const grantType = "authorization_code";
+const appBasePath = process.env.APP_BASE_PATH;
+const callbackPath = "/login/callback";
+const redirectURI = appBasePath + ":" + port + callbackPath;
+
+// Re-route requests to root to /login.
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.redirect("/login");
 });
 
+// Link to trigger authentication.
 app.get("/login", (req, res) => {
-  res.send(
-    "<a href='https://auth.pingone.com/2fb48636-1624-456c-a657-a81a6c30e3ec/as/authorize?response_type=code&client_id=ef8bee74-e572-4fc4-b8ed-cd9530511633&scope=openid%20profile%20email%20address%20p1%3Aread%3Auser&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin%2Fcallback'>Login</a>"
-  );
+  const authzReq = new URL(envID + "/as/authorize", authBasePath);
+
+  authzReq.searchParams.append("response_type", responseType);
+  authzReq.searchParams.append("client_id", clientID);
+  authzReq.searchParams.append("scope", scope);
+  authzReq.searchParams.append("redirect_uri", redirectURI);
+
+  res.send("<a href=" + authzReq.toString() + ">Login</a>");
 });
 
-app.get("/login/callback", async (req, res) => {
+app.get(callbackPath, async (req, res) => {
   const authorizationCode = req.query.code;
+
+  // For demoing purposes
   console.log("");
   console.log("authorizationCode");
   console.log(authorizationCode);
@@ -22,25 +42,26 @@ app.get("/login/callback", async (req, res) => {
   const headers = new Headers();
   headers.append("Content-Type", "application/x-www-form-urlencoded");
 
-  console.log(process.env.AUTHZ_HEADER);
-
-  // Authorization header is the result of base64 encoding client_id + ":" +
-  // client_secret ex. output for the authorization header: "Basic
-  // 01234mVlNzQtZT3Mi00ZmM0WI4ZWQtY2Q5NTMwTE0123456=="
-  headers.append("Authorization", process.env.AUTHZ_HEADER);
+  // Authorization header is the result of base64 encoding the string
+  // (clientID + ":" + clientSecret) appended to "Basic ".
+  // e.g.,
+  // "Basic 0123456lNzQtZT3Mi00ZmM0WI4ZWQtY2Q5NTMwTE0123456=="
+  const authzHeader =
+    "Basic " + Buffer.from(clientID + ":" + clientSecret).toString("base64");
+  headers.append("Authorization", authzHeader);
 
   // Use URLSearchParams because we're using
   // "application/x-www-form-urlencoded".
   const urlBodyParams = new URLSearchParams();
   // The grant type is the OAuth 2.0 grant type that the PingOne app connection
   // is configured to accept.
-  urlBodyParams.append("grant_type", "authorization_code");
+  urlBodyParams.append("grant_type", grantType);
   // Include the authorization code that was extracted from the url when the
   // user was redirected to the redirect_uri from PingOne.
   urlBodyParams.append("code", authorizationCode);
   // The redirect_uri is where the tokens will be sent and must be registered
   // with PingOne by configuring the app connection.
-  urlBodyParams.append("redirect_uri", "http://localhost:3000/login/callback");
+  urlBodyParams.append("redirect_uri", redirectURI);
 
   // Options to supply the fetch function.
   const requestOptions = {
@@ -50,36 +71,30 @@ app.get("/login/callback", async (req, res) => {
     redirect: "follow",
   };
 
-  // Make the exchange for tokens by calling the /token endpoint.
+  const tokenEndpoint = authBasePath + "/" + envID + "/as/token";
+
+  // Make the exchange for tokens by calling the /token endpoint and sending the
+  // authorization code.
   try {
-    const response = await fetch(
-      "https://auth.pingone.com/2fb48636-1624-456c-a657-a81a6c30e3ec/as/token",
-      requestOptions
-    );
+    const response = await fetch(tokenEndpoint, requestOptions);
     const result = await response.json();
 
+    // For demoing purposes
     console.log("");
     console.log("result");
     console.log(result);
     res.json(result);
   } catch (error) {
     // Handle error
+
+    // For demoing purposes
     console.log(error);
     res.send(error);
   }
-
-  //     return null;
-  //   }
-  //   const options = {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization: authHeader,
-  //     },
-  //     redirect: "follow",
-  //   };
-  //   fetch();
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(
+    `App listening on port ${port}. If running locally, navigate to http://localhost:3000 or http://localhost:3000/login in a browser.`
+  );
 });
